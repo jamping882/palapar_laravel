c2c66cf048a7ce3b0629f2d6cf7e7a1d6a10264b
jamping882 <hannajane.blantucas@gmail.com>
2020-09-18 11:46:08 +0800

    first commit

---
+++ resources/views/pages/extra.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Prelim Demo</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Extra Page</h1>
+    <a href="{{url('/')}}">Home</a> | <a href="{{url('/profile')}}">Profile</a> | Extra
+
+</body>
+</html>
---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Prelim Demo</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Profile Page</h1>
+    <a href="{{url('/')}}">Home</a> | Profile | <a href="{{url('/extra')}}">Extra</a>
+
+</body>
+</html>
